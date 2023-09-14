d7398d45093853fb8f7b4be770a93bf3f58bca4b
Amaljith64 <amaljith64@gmail.com>
2023-09-14 22:47:00 +0530

    added deploy.sh

---
+++ apps/client-next/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/version/node/v20.5.1/bin
+
+cd /home/ubuntu/monorepo-github-ci-cd
+git pull origin master
+yarn build
+pm2 stop next
+pm2 start npm --name "next" -- run "start:next
