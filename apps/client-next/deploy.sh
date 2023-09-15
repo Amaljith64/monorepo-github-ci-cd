2591642afcf7fb72a73c2d91b0c4f4215e9f25a2
Amaljith64 <amaljith64@gmail.com>
2023-09-15 11:49:19 +0530

    server changed

--- apps/client-next/deploy.sh
+++ apps/client-next/deploy.sh
@@ -3,7 +3,6 @@ export PATH=$PATH:/home/ubuntu/.nvm/version/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo-github-ci-cd
 git pull origin master
-source ~/.bashrc
 yarn build
 pm2 stop next
 pm2 start npm --name "next" -- run "start:next"
