4ee20a00cc589c54155c7ac973b5c30748827af8
Amaljith64 <amaljith64@gmail.com>
2023-09-15 09:32:11 +0530

    trying fix

--- apps/client-react/deploy.sh
+++ apps/client-react/deploy.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo-github-ci-cd
 git pull origin master
+source ~/.bashrc
 yarn build
 pm2 stop react
 pm2 start npm --name "react" -- run "start:react"
--- apps/express-backend/deploy.sh
+++ apps/express-backend/deploy.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/home/ubuntu/.nvm/version/node/v20.5.1/bin
 
 cd /home/ubuntu/monorepo-github-ci-cd
 git pull origin master
+source ~/.bashrc
 yarn build
 pm2 stop express
 pm2 start npm --name "express" -- run "start:express"
